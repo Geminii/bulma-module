122a4bfa4a21e4e181fceb2d3ce23783b4ada158
Jimmy Jouanne <jimmy.jouanne@gmail.com>
2021-01-18 16:40:25 +0100

    feat: add example

---
+++ example/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World !
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+      <a class="button is-primary">
+        Button
+      </a>
+      <a class="button is-link">
+        Button
+      </a>
+      <a class="button is-info">
+        Button
+      </a>
+      <a class="button is-success">
+        Button
+      </a>
+      <a class="button is-warning">
+        Button
+      </a>
+      <a class="button is-danger">
+        Button
+      </a>
+    </div>
+  </section>
+</template>
